4b76c0dcb6aaaa94f42ea7721592ab64c9b47aed
vanHekthor <35042166+vanHekthor@users.noreply.github.com>
2020-06-09 19:44:23 +0200

    german webpage added

--- javascript/navburger.js
+++ javascript/navburger.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
+    const $languageItem = document.getElementById('switch_language');
 
     // Add a click event on each of them
     $navbarBurgers.forEach( el => {
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
-
+        $languageItem.style.display="none"; 
       });
     });
   }
